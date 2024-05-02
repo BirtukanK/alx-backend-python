441d772461c49c732d75686db8a616d15a7da4a8
BirtukanK <birtukankuma1113@gmail.com>
2024-05-02 20:14:24 +0000

    annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+""" defines add function"""
+def add(a: float, b: float) -> float:
+    """ function to add two functions"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""defines concatination function"""
+def concat(str1: str, str2: str) -> str:
+    """concatinates two strings"""
+    return str1 + str2
