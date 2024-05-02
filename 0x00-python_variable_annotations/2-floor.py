1fe571d283f9b3b54552a28f13216d108766ad71
BirtukanK <birtukankuma1113@gmail.com>
2024-05-02 21:03:58 +0000

    annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" defines annotations"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """ defines floor of a number"""
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' defines annotation'''
+
+
+def to_str(n: float) -> str:
+    '''defines annotation'''
+    return str(n)
