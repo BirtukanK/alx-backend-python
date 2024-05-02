e2e278d6bb7f26e2d7c6fe76d7f7185f2b5270ae
BirtukanK <birtukankuma1113@gmail.com>
2024-05-02 21:24:22 +0000

    annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''defines sum of list'''
+
+
+def sum_list(input_list: float) -> float:
+    ''' defines sum of items in a list'''
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''defines sum of list with different annotations'''
+
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+    '''defines sum of list with different annotations'''
+    return sum(mxd_lst)
