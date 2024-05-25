9c9b9e194fa6adaf82762d925792406f2d0c8706
BirtukanK <birtukankuma1113@gmail.com>
2024-05-25 17:57:13 +0000

    Mock and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,9 +3,10 @@
 """
 
 
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,6 +28,25 @@ class TestAccessNestedMap(unittest.TestCase):
         """tests if path or map is given correctly"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ testing class for getting json data"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+        ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ tests if get_json returns the expected result"""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+
+        mock_get.assert_called_once_with(test_url)
 
 
 if __name__ == '__main__':
